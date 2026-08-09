3f57c5f72fbe816f11227a04a69c858099c6f318
Marco Polo <marco0417@gmail.com>
2026-08-09 22:35:25 +0800

    fix: open FAQ/Privacy in-place (remove _blank), rely on in-page return button (v1.3.55, default skin)

--- assets/circle-help-BjcBW2Cs.js
+++ assets/circle-help-BjcBW2Cs.js
@@ -1,4 +1,4 @@
-import{u as e}from"./index-DpQ64jHk.js";/**
+import{u as e}from"./index-C69dnFxA.js";/**
  * @license lucide-react v0.460.0 - ISC
  *
  * This source code is licensed under the ISC license.
