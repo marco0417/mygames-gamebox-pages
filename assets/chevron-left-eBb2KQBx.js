fc05a516a0c9b0bac921185ad36bf7659c2ad8ac
Marco Polo <marco0417@gmail.com>
2026-09-01 13:02:22 +0800

    v1.3.70：修复凑格子棋盘错位（格子/行目标交错渲染）+ 3 款新教程重来按钮改回图标样式

--- assets/chevron-left-eBb2KQBx.js
+++ assets/chevron-left-eBb2KQBx.js
@@ -1,4 +1,4 @@
-import{t as e}from"./index-BMGTQHGa.js";/**
+import{t as e}from"./index-BgOZk4eR.js";/**
  * @license lucide-react v0.460.0 - ISC
  *
  * This source code is licensed under the ISC license.
